c24d9f6f7a4cca7dca3fb00a32d78c46eaf907f9
IlhamTessa13 <ilhamts139@gmail.com>
2025-07-13 13:26:23 +0700

    Update CORS untuk domain Vercel

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@ return [
 'paths' => ['api/*'],
 'allowed_methods' => ['*'],
 
-'allowed_origins' => ['http://localhost:5173', 'https://frontend-njm8tbxdm-ilhamts139-9696s-projects.vercel.app'],
+'allowed_origins' => ['http://localhost:5173', 'https://frontend-cjozncama-ilhamts139-9696s-projects.vercel.app/'],
 
 'allowed_origins_patterns' => [],
 'allowed_headers' => ['*'],
