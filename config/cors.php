377eb2d149a716788698cedf286652a95f900ec3
IlhamTessa13 <ilhamts139@gmail.com>
2025-07-13 12:46:50 +0700

    Update konfigurasi CORS untuk Vercel

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,13 @@
 <?php
 return [
-    'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => true,
+'paths' => ['api/*'],
+'allowed_methods' => ['*'],
+
+'allowed_origins' => ['http://localhost:5173', 'https://frontend-gc3ew89ru-ilhamts139-9696s-projects.vercel.app'],
+
+'allowed_origins_patterns' => [],
+'allowed_headers' => ['*'],
+'exposed_headers' => [],
+'max_age' => 0,
+'supports_credentials' => true,
 ];
